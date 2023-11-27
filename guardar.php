d9a09491120e4970b6e61bd2050175282295a700
enma11235 <143436386+enma11235@users.noreply.github.com>
2023-11-27 13:22:20 -0300

    modificacion buscar.php

--- guardar.php
+++ guardar.php
@@ -1,7 +1,18 @@
 <?php
+
+//debo asegurarme de que el usuario no existe en users.txt
+
 header("Location: index.html");
-$nombre = $_POST["nombre"];
+$nombre = argv[1];
 $fp = fopen("users.txt", "a");
 fwrite($fp, $nombre."\n");
 fclose($fp);
+
+if (mkdir("/users/{$nombre}/notas", 0777, true)) {
+    //crear menu.html con javascript recibiendo $nombre como parametro, y tambien una nota de ejemplo.
+} else {
+    echo "Hubo un error al intentar crear el archivo.";
+}
+
+
 ?>
